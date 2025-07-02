389fc562c0e0c591aeb321f12a7d5528a31d3ebb
RenanZavagli <renanfzc@gmail.com>
2025-07-02 17:09:11 -0300

    Entrega Trilha Java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //TODO: Exibir as mensagens para nosso usuário
+        System.out.println("Por favor digite o numero da conta!");
+        System.out.print("Usuário: ");
+        //TODO: Obter pela classe Scanner os valores digitados no terminal
+        int numero = scanner.nextInt();
+
+        System.out.println();
+
+        System.out.println("Por favor digite o numero da Agência!");
+        System.out.print("Usuário: ");
+        String agencia = scanner.next();
+
+        System.out.println();
+
+        System.out.println("Por favor digite o nome do Cliente!");
+        System.out.print("Usuário: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println();
+
+        System.out.println("Por favor digite o Saldo!");
+        System.out.print("Usuário: ");
+        double saldo = scanner.nextDouble();
+
+        //TODO: Exibir a mensagem da conta criada
+
+        System.out.println("Olá " + nomeCliente +" obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +
+        " conta "+ numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
